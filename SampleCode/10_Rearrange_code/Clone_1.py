3d00d7db463753413cc3ca4eb106aef39fabbebc
Chalermwat <zi.sk134@gmail.com>
2024-02-07 23:26:16 -0500

    More code rearragement sampleCode

---
+++ SampleCode/10_Rearrange_code/Clone_1.py
@@ -0,0 +1,11 @@
+Welcome_text='# Welcome #'
+sumsumsumsumsumsumsumsumsumsumsumsumsumsumsum=0
+round=10
+End_textEnd_textEnd_textEnd_textEnd_textEnd_text="### End ###"
+
+print(Welcome_text)
+for i in range(round):
+    sum+=i
+
+print(sumsumsumsumsumsumsumsumsumsumsumsumsumsumsum)
+print(End_textEnd_textEnd_textEnd_textEnd_textEnd_text)
